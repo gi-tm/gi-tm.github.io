409b33a42391151ef71ebd5a24b9a9d6ae18889f
Gloria Mbumba <77781959+gi-tm@users.noreply.github.com>
2024-11-19 00:31:54 -0800

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,6 +3,6 @@ session_start();
 define('ROOT_URL', 'http://localhost/Blog/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'gitm');
-define('DB_PASS', '1163');
+define('DB_PASS', 'hehe');
 define('DB_NAME', 'blog');
 
